f8e2b672410b20118066d5abe9a04077a5b46782
Bastien de Luca <bastien.deluca@digitregroup.com>
2021-03-04 12:58:33 +0100

    Fix faulty zsh install

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+# Change shell to zsh
+chsh -s /bin/zsh
+
 # Link .zshrc
 rm -rf $HOME/.zshrc
-ln -s $HOME/.dot/zsh/.zshrc $HOME/.zshrc
+ln -s $HOME/.dotfiles/zsh/.zshrc $HOME/.zshrc
 
 # Link .antigenrc
 rm -rf $HOME/.antigenrc
-ln -s $HOME/.dot/zsh/.antigenrc $HOME/.antigenrc
+ln -s $HOME/.dotfiles/zsh/.antigenrc $HOME/.antigenrc
